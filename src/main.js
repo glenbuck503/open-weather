165805e8b1f60d8760d94d952d5659cabb11a7f6
glenbuck503 <glenbuck503@gmail.com>
2020-11-17 11:13:25 -0800

    refactor UI and business logic
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import WeatherService from './weather-service.js'
+import WeatherServiceZip from './weather-service-zip.js'
 
 function clearFields() {
   $('#location').val("");
   $('.showErrors').text("");
   $('.showHumidity').text("");
   $('.showTemp').text("");
+  $('.showWindSpeed').text("");
 }
 
 $(document).ready(function() {
@@ -20,41 +22,24 @@ $(document).ready(function() {
       const body = JSON.parse(response);
       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.showWindSpeed').text(`The wind speed is ${body.wind.speed} mph.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+
+  $('#zipCode').click(function () {
+    $(this).data('clicked', true);
+    let zip = $('#zipcode').val();
+    clearFields();
+    let promise = WeatherServiceZip.getWeather(zip);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${zip} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.showWindSpeed').text(`The wind speed is ${body.wind.speed} mph.`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
   });
 });
-
-
-
-
-
-
-
-
-
-  // $('#zipCode').click(function () {
-  //   const zip = $('#zipcode').val();
-  //   $('#zipcode').val("");
-
-  //   let request = new XMLHttpRequest();
-  //   const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${process.env.API_KEY}`;
-
-  //   request.onreadystatechange = function () {
-  //     if (this.readyState === 4 && this.status === 200) {
-  //       const response = JSON.parse(this.responseText);
-  //       getElements(response);
-  //     }
-  //   }
-
-  //   request.open("GET", url, true);
-  //   request.send();
-
-  //   function getElements(response) {
-  //     $('.showHumidity2').text(`The humidity in ${zip} is ${response.main.humidity}%`);
-  //     $('.showTemp2').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-  //     $('.showWindSpeed2').text(`The wind speed is ${response.wind.speed} mph.`);
-      
-  //   }
-  // });
